6b875c7de158a27c6d3fcbf6e33c26eb3e103301
Suhail Manzoor <93788026+SuhAilMAnzoor@users.noreply.github.com>
2024-05-05 11:45:15 -0700

    Add files via upload

---
+++ 01.Basics Of Dart/Basics Of Dar.dart
@@ -0,0 +1,47 @@
+void main(){
+  
+  print("SohAil MAnzoor");
+
+  print(12);
+
+  print("2+2");
+
+  print(2+2);
+
+
+//      Data Type   Variable Name    Data For Storing
+//>>    String
+        String          myname =       "SohAil MAnzoor";
+        String          Email  =    "sohailmanzoor@gmail.com";
+        String          Address =  "Badah City Dis Larkana"; 
+
+//>>   Integer 
+        int             age  =          26;   //Corret
+//      int             age  =         "26";  //Wrong
+//      int             age  =          26.0; //Wrong
+
+//>>   Double
+       double          age1 =          26.0;   //correct
+       num             age2 =          26.0;   //correct
+       num             age3 =          26;     //correct
+
+    // we use num when we are not sure about what kind of number will
+    // will be stored
+    
+      double age4 = age;      // we cant do this ! integer can not be assigned to double
+
+// Variable  is used  for storing data 
+  var name = "SohAil";
+  var myage = 3;         // use int for integer number
+  var percentage = 3.4;  // use double for decmal number
+  // to restrict values in var we use data type to restrict values
+
+  // use proper names for variable for what we need 
+  // like for name we use name variable 
+  // for age we use age variable name 
+  // good practice
+
+
+// lecture 1 complete
+
+}
